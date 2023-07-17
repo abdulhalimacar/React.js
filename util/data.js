47af275dfc32b05a1f9702af669611d6e732a24d
Abdulhalim acar <109023093+abdulhalimacar@users.noreply.github.com>
2023-07-17 22:01:27 +0300

    Add files via upload

---
+++ util/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id:1,
+        language:"JS",
+        img: "https://tse1.mm.bing.net/th?id=OIP.Hf1fzVhQli-QEEdHhyDydwHaIr&pid=Api&P=0&h=180",
+        btn: "Info",
+    },
+
+    {
+        id:2,
+        language:"CSS",
+        img: "https://tse1.mm.bing.net/th?id=OIP.Hf1fzVhQli-QEEdHhyDydwHaIr&pid=Api&P=0&h=180",
+        btn: "Detail",
+    },
+
+    {
+        id:3,
+        language:"HTML",
+        img: "https://tse1.mm.bing.net/th?id=OIP.Hf1fzVhQli-QEEdHhyDydwHaIr&pid=Api&P=0&h=180",
+        btn: "More",
+    },
+];
+
+export default data; //bu umuma/genele açmak demektir...
---
+++ util/data2.js
@@ -0,0 +1,37 @@
+const data2 = [
+
+    {
+        id:1,
+        name:"Ali Mehmet",
+        job:"Developer",
+        comment:"it was great!",
+        img:"https://1000marcas.net/wp-content/uploads/2020/11/JavaScript-logo-1536x960.png",
+    },
+
+    {
+          id:2,
+        name:"veli Mehmet",
+        job:"data",
+        comment:"it was not great!",
+        img:"https://1000marcas.net/wp-content/uploads/2020/11/JavaScript-logo-1536x960.png",
+    },
+
+    {
+        id:3,
+        name:"Ali ata Mehmet",
+        job:"Developer sales",
+        comment:"it was tooo great!",
+        img:"https://1000marcas.net/wp-content/uploads/2020/11/JavaScript-logo-1536x960.png",
+    },
+
+    {
+        id:4,
+        name:"tarık ata Mehmet",
+        job:"Developer sales machine",
+        comment:"it was tooo great!",
+        img:"https://1000marcas.net/wp-content/uploads/2020/11/JavaScript-logo-1536x960.png",
+    },
+];
+
+export default data2;
+
