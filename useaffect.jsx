5e3624739062e11636598e23da2f928508d826f0
ahalim <acar45671@gmail.com>
2023-07-22 08:06:59 -0700

    mount done

---
+++ useaffect.jsx
@@ -0,0 +1,49 @@
+import {useState, useEffect} from "react";
+ 
+
+const UseEffectHook = ()=>{
+
+    const [count,setCount] = useState(0);
+
+    //burada component did mount yaptık//her sayfa yenilendiğinde yani her doğumda mounting çalışır
+    //fetch,async-await, localstorage,settimeout,setinterval
+
+    //burada biz mountig ile data çekme simülasyonu yaptık:componendidmount yaptık
+    // useEffect(() => {
+    //   console.log("mouutn");
+    //   setTimeout(()=>{
+    //     alert("data fetched");
+    //   },3000);
+
+
+    // },[]); burada [bu dependencyi yeridir] eğer dependicy boşsa component did mount kullanılır  
+
+    //componendidupdate yapacaz: ama burası didmount ile kullanılır: dolayısıyla dependincyinin yerinin boş olmaması lazım...
+    useEffect(() => { 
+      console.log("mounting+updating oldu burası artık");
+      setTimeout(() => {
+        alert("data fetched");
+      }, 3000);
+    }, [count]);
+    
+    
+
+    const handleinc = ()=>{
+
+        setCount(count+1);
+    }
+
+    return (
+ 
+    <div className="container text-center">
+        <h1 className="text-danger">useeffect</h1>
+        <h3>count={count}</h3>
+        <button className="btn btn-info" onClick={handleinc}>
+            inc
+        </button>
+    </div>
+  
+)
+};
+
+export default UseEffectHook;
---
+++ user.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+const User = ()=>{
+  const [user, setUser] = useState(""); 
+
+  const getUser = () => { 
+    fetch("https://randomuser.me/api/")
+      .then((res) => res.json())
+      .then((data) => setUser(data.results[0]));
+  };
+
+  useEffect(() => {
+    //componentdidmount yaptık burada...
+    getUser(); 
+  }, []);
+
+  //{name && name.first}>name true ise name.first'i al = {name?.first} ikisi aynıdır
+  //{name?.first?.x} name varsa firstünü getir eğer first te varsa x'i de getir dedik burada
+
+  console.log(user);
+
+  const { name, dob, email, picture } = user;
+  return (
+    <div>
+      <h1>{name && name.first}</h1>
+      <img className="rounded-circle" src={picture?.large} alt="" />
+      <p>{email}</p>
+      {/* <h5>{dob?.date} {dob?.age}</h5> */}
+      <h5>
+        {new Date(dob?.date).toLocaleDateString("ar-SA")}
+      </h5>
+
+      <button className="btn btn-warning" onClick={getUser}>
+        get user
+      </button>
+    </div>
+  );
+};
+
+export default User;
+
